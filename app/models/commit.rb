2b7351e7cd61b31eb923b9ef9f69f8e0473197f8
Eduardo Del Balso <e.delbalso@gmail.com>
2012-05-16 13:20:48 -0700

    a bit of cleanup

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,14 +2,14 @@ require 'grit'
 
 class Commit < ActiveRecord::Base
 
+  PATH_TO_REPO = File.join('~','Dev','codereview')
+
   scope :new_commits, where("is_new == ?",true)
   scope :rejected, where("is_new == ? AND accepted <> ?",false,true)
 
   def self.sync
     commits = Commit.all.map(&:refid)
-    #repo = Grit::Repo.new('/Users/modcloth/workspace/codereview')
-    #new_commits = repo.commits('sphere_dsc_filter',20).select{|c| c.message.include? '[DSC]'}.reject{|c| commits.include? c.id}
-    repo = Grit::Repo.new('~/Dev/codereview')
+    repo = Grit::Repo.new(PATH_TO_REPO)
     new_commits = repo.commits('master',20).reject{|c| commits.include? c.id}
 
     new_commits.each{ |c|
