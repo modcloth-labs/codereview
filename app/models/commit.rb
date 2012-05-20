48cd40b05bfc54d264b0e6449026c27aab809e1a
Juris Galang <jurisgalang@gmail.com>
2012-05-19 19:05:52 -0700

    Added spec for Commit model; /path/to/repo is now configurable

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,7 +1,7 @@
 require 'grit'
 
 class Commit < ActiveRecord::Base
-  PATH_TO_REPO = '~/code/modcloth/codereview'
+  PATH_TO_REPO = YAML.load(File.read 'config/git.yml')[:path]
 
   scope :new_commits, where("is_new == ?", true).order('commit_date desc')
   scope :rejected,    where("is_new == ? AND accepted <> ?", false, true).order('commit_date desc')
@@ -9,12 +9,8 @@ class Commit < ActiveRecord::Base
   def self.sync
     commits     = Commit.all.map(&:refid)
     repo        = Grit::Repo.new(PATH_TO_REPO)
-    new_commits = repo.commits('master', 20).reject{ |c| 
-      puts "x #{c.message}"
-      commits.include? c.id 
-    }
-
-    new_commits.each{ |c|
+    new_commits = repo.commits('master', 20).reject{ |c| commits.include? c.id }
+    new_commits.each do |c|
       Commit.new(
         :refid       => c.id,
         :author      => c.author.name,
@@ -23,7 +19,7 @@ class Commit < ActiveRecord::Base
         :accepted    => nil,
         :is_new      => true
       ).save
-    }
+    end
   end
 
   def self.restart
